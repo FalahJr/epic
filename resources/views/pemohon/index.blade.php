bd475ec676d98afc3355c8ec98b220ba972b9429
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-11-20 00:11:10 +0700

    Feat (backend) : slicing halaman pemohon

--- resources/views/pemohon/index.blade.php
+++ resources/views/pemohon/index.blade.php
@@ -11,7 +11,7 @@
   <div class="row">
     <div class="col-lg-12">
       <nav aria-label="breadcrumb" role="navigation">
-        <ol class="breadcrumb bg-warning">
+        <ol class="breadcrumb bg-warning" style="background-color: #499DB1 !important">
           <li class="breadcrumb-item"><i class="fa fa-home"></i>&nbsp;<a href="/home">Home</a></li>
           {{-- <li class="breadcrumb-item">Setup Master Tagihan</li> --}}
           <li class="breadcrumb-item active" aria-current="page">Pemohon</li>
@@ -29,7 +29,7 @@
                     </div>
                     <div class="table-responsive">
         				        <table class="table table_status table-hover " id="table-data" cellspacing="0">
-                            <thead class="bg-warning text-white">
+                          <thead class="bg-warning text-white" style="background-color: #499DB1 !important">
                               <tr>
                                 <th>No</th>
                                 <th>Username</th>
